fe7abfa86795013a97d59d9d955f4e5fc3116a5b
AsienGz <163406960+AsienGz@users.noreply.github.com>
2024-12-06 17:09:29 +0100

    Update machine_learning.py

--- machine_learning.py
+++ machine_learning.py
@@ -1,9 +1,9 @@
-#This is the main code, which uses implemented machine learning, created in the file train_model.py
-#User needs to install tensorflow
+# This is the main code, which uses implemented machine learning, created in the file train_model.py
+# User needs to install tensorflow
 
 
 import streamlit as st
-from tensorflow.keras.models import load_model #Tensorflow is an open source framework that we chose to use for image recognition because it provides tools and libraries for developing machine learning models
+from tensorflow.keras.models import load_model # Tensorflow is an open source framework that we chose to use for image recognition because it provides tools and libraries for developing machine learning models
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
 from PIL import Image
@@ -14,33 +14,34 @@ import os
 
 def get_cocktail_recipe(cocktail_name):
     API_KEY = '9973533'  # API Key Pro Version
-    BASE_URL = f"https://www.thecocktaildb.com/api/json/v2/{API_KEY}/search.php?s={cocktail_name}" #API Pro Version URL
+    BASE_URL = f"https://www.thecocktaildb.com/api/json/v2/{API_KEY}/search.php?s={cocktail_name}" # API Pro Version URL
     response = requests.get(BASE_URL) # Making the API request
-    if response.status_code == 200: #Checks if the API request was successfull 
+    if response.status_code == 200: # Checks if the API request was successfull 
         data = response.json() 
         if data['drinks']:
             drink = data['drinks'][0] #First string of the list
             instructions = drink['strInstructions'] # Retrieves the recipe for the drink
             drink_thumb = drink['strDrinkThumb']  # Image URL of the cocktail
-            return instructions, drink_thumb
-    return None, None  # Return None if no recipe is found
+            return instructions, drink_thumb # Returns the recipe and the image URL as a tuple
+    return None, None  # Returns None if no recipe is found
 
 # Load custom-trained model
 
-custom_model_path = "/Users/asya/Downloads/CocktailScanner/cocktail_model.keras"
-label_map_path = "/Users/asya/Downloads/CocktailScanner/label_map.txt"
+custom_model_path = "/Users/asya/Downloads/CocktailScanner/cocktail_model.keras" # Product of the train_model.py which recognizes 7 different cocktails
+label_map_path = "/Users/asya/Downloads/CocktailScanner/label_map.txt" # Lists the cocktails, that can be recognized
 
-if os.path.exists(custom_model_path) and os.path.exists(label_map_path):
-    custom_model = load_model(custom_model_path)
-    labels = {}
+if os.path.exists(custom_model_path) and os.path.exists(label_map_path): # Checks if the file exists
+    custom_model = load_model(custom_model_path) # Loads the model 
+    labels = {} # Empty dictionnary to store labels
     with open(label_map_path, "r", encoding="utf-8") as f:
         for line in f:
-         key, value = line.strip().split(":")
-         labels[int(key)] = value
+         key, value = line.strip().split(":") # Strips whitespace and splits the line at the colon
+         labels[int(key)] = value # Converts the key to int and stores it in the labels dictionary
 else:
-    raise FileNotFoundError("Custom model or label map file not found!")
+    raise FileNotFoundError("Custom model or label map file not found!") # Error if file not found
 
 # Streamlit app
+
 st.title("Cocktail Image Recognition")
 st.write("Upload an image of a cocktail to identify it!")
 
